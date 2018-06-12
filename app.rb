43a6dc82d9dbdcc1ff6d57dc9605174e1d8624e8
amemonkey <ameliafunaki789@gmail.com>
2018-06-12 17:57:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "Hello, Ame."
+  end
+  
+  get '/hometown' do
+    "My hometown is Stansbury Park, UT"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is 'Questing, Questing' by the Backyardigans."
+  end
 end
+
+
